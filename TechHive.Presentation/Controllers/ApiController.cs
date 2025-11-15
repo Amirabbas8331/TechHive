fa4c35de4325e9affd76afd2dcef97019f74988d
Amirabbas8331 <amirabbas8331@gmail.com>
2025-11-15 11:56:47 +0330

    Complete CQRS and MediatR

---
+++ TechHive.Presentation/Controllers/ApiController.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using TechHive.Domain;
+
+namespace TechHive.Presentation.Controllers;
+
+[ApiController]
+public class ApiController : ControllerBase
+{
+    protected IActionResult Problem(List<Error> errors)
+    {
+        if (errors.Count is 0)
+        {
+            return Problem();
+        }
+
+        if (errors.All(error => error.Type == ErrorType.Validation))
+        {
+            return ValidationProblem(errors);
+        }
+
+        return Problem(errors[0]);
+    }
+
+    protected IActionResult Problem(Error error)
+    {
+        var statusCode = error.Type switch
+        {
+            ErrorType => StatusCodes.Status409Conflict,
+            _ => StatusCodes.Status500InternalServerError,
+        };
+
+        return Problem(statusCode: statusCode, detail: error.Description);
+    }
+
+    protected IActionResult ValidationProblem(List<Error> errors)
+    {
+        var modelStateDictionary = new ModelStateDictionary();
+
+        foreach (var error in errors)
+        {
+            modelStateDictionary.AddModelError(
+                error.Code,
+                error.Description);
+        }
+
+        return ValidationProblem(modelStateDictionary);
+    }
+}
---
+++ TechHive.Presentation/Controllers/ProductsController.cs
@@ -0,0 +1,56 @@
+﻿
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TechHive.Application.Products.Command.CreateProducts;
+using TechHive.Application.Products.Command.DeleteProducts;
+using TechHive.Application.Products.Command.UpdateProducts;
+using TechHive.Application.Products.Query.GetProducts;
+using TechHive.Domain.ValueObjects;
+
+
+namespace TechHive.Presentation.Controllers;
+
+[Route("[controller]")]
+public class ProductsController : ApiController
+{
+    private readonly ISender _sender;
+
+    public ProductsController(ISender sender)
+    {
+        _sender = sender;
+    }
+    [HttpPost]
+    public async Task<IActionResult> CreateProduct(CreateProductCommand request)
+    {
+        var command = new CreateProductCommand(request.Name,request.Code,request.Price,request.Status,request.Description);
+        var CreateProductresult = await _sender.Send(command);
+        return Ok(CreateProductresult);
+
+    }
+    
+    [HttpDelete("{ProductId:int}")]
+    public async Task<IActionResult> DeleteProduct(ProductId productId)
+    {
+        var command = new DeleteProductCommand(productId);
+        var DeleteProductresult = await _sender.Send(command);
+        return NoContent();
+
+    }
+    [HttpGet("{productId:guid}")]
+    public async Task<IActionResult> GetProduct(ProductId productId)
+    {
+        var query = new GetProductQuery(productId);
+        var GetProductResult = await _sender.Send(query);
+        return Ok(GetProductResult);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateProduct(UpdateProductCommand command)
+    {
+        var query = new UpdateProductCommand(command.Product);
+        var UpdateProductresult = await _sender.Send(query);
+        return UpdateProductresult.IsSuccess?Ok(UpdateProductresult):Problem();
+
+    }
+ 
+}
